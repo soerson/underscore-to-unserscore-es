6824a43d2e0c1ece2b5fe544bc9a3ee5bef8a768
soer <soerson@pm.me>
2019-04-04 15:30:06 +0300

    Recursivelly go through directories

---
+++ index.js
@@ -0,0 +1,31 @@
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+//joining path of directory 
+const directoryPath = path.join(__dirname, '../bma/src/app/account');
+//passsing directoryPath and callback function
+//
+const deepReadDir = function (pathToDir, cb) {
+    return fs.readdir(pathToDir, {withFileTypes: true}, function (err, files) {
+        //handling error
+        if (err) {
+            cb(err);
+        }
+        //listing all files using forEach
+        files.forEach((dirent) => {
+            if (dirent.isDirectory()) {
+                return deepReadDir(path.join(pathToDir, dirent.name), cb);
+            }
+            // Do whatever you want to do with the file
+            cb(null, dirent);
+        });
+    });
+};
+
+deepReadDir(directoryPath, function (err, file) {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log(file.name);
+});
